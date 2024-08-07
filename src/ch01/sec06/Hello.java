f89ebdd945dacdb8dad6ca41ec8fb2aa5f9007ad
ssssRssssungjune <ssungjune0610@gmail.com>
2024-08-07 12:30:30 +0900

    변경

--- src/ch01/sec06/Hello.java
+++ src/ch01/sec06/Hello.java
@@ -3,7 +3,7 @@ package ch01.sec06;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello java!!으아아아아");
+		System.out.println("HI Hello java!!으아아아아");
 
 	}
 
